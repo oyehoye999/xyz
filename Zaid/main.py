9064608554be6ab8b99deec614773f1a00469a50
oyehoye999 <108613848+oyehoye999@users.noreply.github.com>
2022-07-05 10:51:28 +0530

    Update main.py

--- Zaid/main.py
+++ Zaid/main.py
@@ -8,17 +8,17 @@ from config import API_HASH, API_ID, BOT_TOKEN, SESSION_NAME
 from pyrogram import Client
 from pytgcalls import PyTgCalls
 
-bot = Client(
+bot = Client(5329681433)
     ":memory:",
     API_ID,19360347
     API_HASH,b73dee54ba45d3f9d1480f1be9f1339b
-    bot_token=BOT_TOKEN,
+    bot_token=BOT_TOKEN,5468931327:AAGtFTfw42qHobf91iQD-6MWoZPZ55PnXnc
     plugins={"root": "Zaid.Player"},
 )
 
 BOT = TelegramClient('BOT', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
 
-user = Client(
+user = Client(5329681433)
     SESSION_NAME,BQDCjr4AT_-nALZTn_ug6jF_dwrwW_E5dDweBuK4ZLi1xyqJDJFGt09WzMn8-1DTVphS2ulRAK7JJFQE0Py0IWbskVnDbkRiFBDWW7Q989zpNIkTZOuK0koESI5gsY7ChviWE49xzF9NaMacO-p8xRSaWbBzzu6JOGXqukQRlqgHwqJhROTkrzcjIKj_szaC1SZW3Vx0G5p666J4bTHbyoOH78uWpuMwPEw4gR8rB5YJLIac0ns51JfLX_hjYc3xi4WkNmim76KbGEfRXu9v_aqqMRAHD1ig6TGN1Ku5OESNwrNMzXfHEFMApQDjGeCkFCrI3g-SCTb399_1FqRK4r_8AAAAAT2sfBkA
     api_id=API_ID,19360347
     api_hash=API_HASH,b73dee54ba45d3f9d1480f1be9f1339b
